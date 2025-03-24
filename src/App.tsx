9061315a2162c6bf34945345d2fd61d8ff5c84f1
paulomachava <machava.pauloxavier@gmail.com>
2025-03-24 14:24:11 +0200

    installing react qujery

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import {Toaster} from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
 
@@ -11,7 +13,9 @@ export function App() {
   <>
   <ThemeProvider storageKey='pizzashop-theme' defaultTheme='system'>
     <Toaster richColors />
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
       </>
     
